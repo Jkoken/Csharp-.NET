f3dec52007f6cb3413ed0ef15c24d1691a2890c1
Jackson Wandekoken <48889126+Jkoken@users.noreply.github.com>
2024-12-20 14:43:31 -0300

    Update esfera.cs

--- Desafios Iniciais/Desafio Esfera/esfera.cs
+++ Desafios Iniciais/Desafio Esfera/esfera.cs
@@ -10,7 +10,7 @@ class DIO {
     volume = pi * (4.0/3.0) * Math.Pow( raio , 3 ); //Não foi utilizado o Math.Round(... ,3) pois em caso de retornos com valor zero após a vírgula, ele não iria trazer todas as casas, por exemplo ao desejar a saída "10.440", ele não funcionaria dessa forma, a saída seria "10.44"
     
     Console.WriteLine($"VOLUME = {volume.ToString("F3")}");
-    //Acima foi utilizado o .ToString("F3"), pois era necessário, para o desafio, tarzer três casas decimais.
+    //Acima foi utilizado o .ToString("F3"), pois era necessário, para o desafio, trazer, SEMPRE, três casas decimais, com isso esse comando foi encorpado na saída.
 
   }
 
