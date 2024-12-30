b4db1b80779ddb2c87d2151d82d9d7d48f97bc4d
Jackson Wandekoken <48889126+Jkoken@users.noreply.github.com>
2024-12-30 15:29:11 -0300

    Add files via upload

---
+++ PersonCRUD_WebAPI/PersonCRUD/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonCRUD.Models
+{
+    public class PersonModel
+    {
+        public Guid Id { get; init; } //
+        public string Name { get; set; } = string.Empty; //uma das formas de inicializar a string, falando que ela pode ser nula, com o "string.Empty"?;
+
+    }
+}
---
+++ PersonCRUD_WebAPI/PersonCRUD/Routes/PersonRoute.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonCRUD.Routes
+{
+    public static class PersonRoute //Pode ser static pois não precisa instanciar, deixa pela própria aplicação instanciá-la
+    { //Se a classe é estática, todos os membros devem ser estáticos
+        public static void PersonRoutes(this WebApplication app) //Utilizando o "this" para tornar o método um "Extension Method".
+        {
+            app.MapGet("person", () => "Hello World!"); //primeiro parâmetro > ROTA | segundo parâmetro > Handler (como ele vai manusear a rota)
+        }
+    }
+}
